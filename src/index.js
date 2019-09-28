4e6a9b93ffa88177b55cf59f2729d09b32f7c091
Erik Sánchez <erik.sape@gmail.com>
2019-09-28 11:49:12 -0500

    reto 2

--- src/index.js
+++ src/index.js
@@ -3,30 +3,30 @@ const $observe = document.getElementById('observe');
 const API = 'http://us-central1-escuelajs-api.cloudfunctions.net/characters';
 
 const getData = api => {
-  fetch(api)
+  return fetch(api)
     .then(response => response.json())
-    .then(response => {
-      localStorage.setItem('next_fetch', response.info.next);
-      console.log(`next_fetch ${localStorage.getItem('next_fetch')}`);
-      const characters = response.results;
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  const apiUrl = localStorage.getItem('next_fetch') ? localStorage.getItem('next_fetch') : API;
+  const response = await getData(apiUrl);
+
+  localStorage.setItem('next_fetch', response.info.next);
+  console.log(`next_fetch ${localStorage.getItem('next_fetch')}`);
+  const characters = response.results;
+  let output = characters.map(character => {
+    return `
+  <article class="Card">
+    <img src="${character.image}" />
+    <h2>${character.name}<span>${character.species}</span></h2>
+  </article>
+`
+  }).join('');
+  let newItem = document.createElement('section');
+  newItem.classList.add('Items');
+  newItem.innerHTML = output;
+  $app.appendChild(newItem);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
